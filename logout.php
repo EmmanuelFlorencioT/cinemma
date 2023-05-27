8447a7e2bd1d5277629004742fbe123ad2a49001
EmmanuelFlorencioT <emm.ftr2@gmail.com>
2023-05-27 17:38:33 -0600

    Fixed some logout session errors

--- logout.php
+++ logout.php
@@ -4,6 +4,11 @@
     $_SESSION['logged_in'] = false;
     unset($_SESSION['username']);
     unset($_SESSION['email']);
+    if(isAdmin()){
+        unset($_SESSION['admin_id']);
+    } else {
+        unset($_SESSION['user_id']);
+    }
 
     session_destroy();
 
